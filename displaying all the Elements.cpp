d5568dfa10579a70f228ccbeb69d1d82e27384f3
HEX <badrulnisakhan@icloud.com>
2026-03-18 14:51:41 +0500

    Update displaying all the Elements.cpp
    i cout the elements inside, which is why it was displaying loop elemets along with elements

--- displaying all the Elements.cpp
+++ displaying all the Elements.cpp
@@ -2,10 +2,11 @@
 using namespace std;
 int main()
 {
+    cout << "Elements: " << endl;
 	int array[5] = { 10, 20, 30, 40, 50 };
 	for (int i = 0; i < 5; i++)
 	{
-		cout << "Elements: " << array[i] << endl; // displaying all elements thru the loop
+		cout << array[i] << endl ; //displaying all the elements with for loop
 	}
 }
 	
