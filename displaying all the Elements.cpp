0dc9cfa7f1624eab5f1d9e2bf227ace7f51c7a86
HEX <badrulnisakhan@icloud.com>
2026-03-18 14:34:13 +0500

    used for loop to print elements

---
+++ displaying all the Elements.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int array[5] = { 10, 20, 30, 40, 50 };
+	for (int i = 0; i < 5; i++)
+	{
+		cout << "Elements: " << array[i] << endl; // displaying all elements thru the loop
+	}
+}
+	
