cd34cdd4de78bfc9193a424023ceda30b580c700
HEX <badrulnisakhan@icloud.com>
2026-03-22 01:38:07 +0500

    Find the lowest number?
    with this code you can find the lowest number in an array

---
+++ To find the lowest number in an array.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int array[5] = { 10, 20, 30, 40, 50 };
+	int LowestArray = array[0];
+	for (int i = 0; i < 5; i++)
+	{
+		if (array[i] < LowestArray) 
+		{
+			LowestArray = array[i];
+		}
+	}
+	cout << "Lowest Array: " << LowestArray;
+}
